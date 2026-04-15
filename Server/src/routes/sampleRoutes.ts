2c8bb5e4b76fa6fc3fbe21689fc3033e959b0f9d
Justin <justing0210@gmail.com>
2026-04-15 18:07:04 +0800

    ref: remove sample routes in favor of domain routes

--- Server/src/routes/sampleRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sample } from "../controllers/sample.controller.js";
-
-const router = express.Router();
-
-router.get("/message", sample);
-
-export default router;
