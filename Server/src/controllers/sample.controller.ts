309fcc3b16ad71515cbe6bc60ed26b192b89dacc
Justin <justing0210@gmail.com>
2026-04-15 18:07:03 +0800

    ref: remove sample controller in favor of domain structure

--- Server/src/controllers/sample.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-
-
-export const sample = (req: Request, res: Response) => {
-  res.json({ message: "This is a sample response from the controller." });
-}
