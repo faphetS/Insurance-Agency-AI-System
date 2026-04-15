1dc5753694f200d358c63a4f5229b6143aa91d52
Justin <justing0210@gmail.com>
2026-04-15 18:07:02 +0800

    build: update Server ESLint config

--- Server/eslint.config.js
+++ Server/eslint.config.js
@@ -12,6 +12,7 @@ export default defineConfig([
         "error",
         { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
+      "@typescript-eslint/no-namespace": "off",
     },
   },
 ]);
